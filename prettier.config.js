e92145f048df24dc50c33481f7694a6de137754c
Max <mdiazn@uni.pe>
2025-02-12 17:17:19 -0500

    chore: add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'always',
+  endOfLine: 'auto',
+  singleAttributePerLine: false,
+};
+
+export default config;
